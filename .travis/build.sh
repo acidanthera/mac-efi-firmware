28040a3f1df6c1d53bf5847bf2f58dfd9d85f75a
Download-Fritz <download-fritz@outlook.com>
2016-01-28 20:47:09 +0100

    Add Travis CI build support for Linux GCC and CLANG. Extended the [BuildOptions] section of the platform descriptor file to allow ARM and AARCH64 builds. Expose the EFI architecture the EDK-way.

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-if [ "${CC}" = "clang" ]; then
-  if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    build -t XCODE5 -b DEBUG -b RELEASE -a IA32 -a X64 -p AppleModulePkg/AppleModulePkg.dsc
+if [ "$CC" = "clang" ]; then
+  if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+    build -t XCODE5 -b DEBUG -b RELEASE -a IA32 -a X64 -p AppleModulePkg/AppleModulePkg.dsc -q
   else
-    build -t CLANG35 -b DEBUG -b RELEASE -a IA32 -a X64 -p AppleModulePkg/AppleModulePkg.dsc
+    build -t CLANG35 -b DEBUG -b RELEASE -a AARCH64 -p AppleModulePkg/AppleModulePkg.dsc -q
   fi
-elif [ "${CC}" = "gcc" ]; then
-  build -t GCC49 -b DEBUG -b RELEASE -a IA32 -a X64 -p AppleModulePkg/AppleModulePkg.dsc
+elif [ "$CC" = "gcc" ]; then
+  build -t GCC49 -b DEBUG -b RELEASE -a ARM -a IA32 -a AARCH64 -a X64 -p AppleModulePkg/AppleModulePkg.dsc -q
 fi
--- .travis/install.sh
+++ .travis/install.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-  sudo svn co https://llvm.org/svn/llvm-project/llvm/trunk/include/llvm-c/ /usr/include/llvm-c/ -q
-  cd /tmp
-  git clone https://github.com/opensource-apple/cctools
+if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+  cd /tmp/
+  svn co https://llvm.org/svn/llvm-project/llvm/trunk/include/llvm-c/ /usr/local/include/llvm-c/ -q
+  git clone https://github.com/opensource-apple/cctools.git
   cd cctools
-  set +e # the build of a library not used by this project usually fails.
-  make
+  set +e  # the build of a library not used by this project usually fails.
+  make -s
   set -e
   cd efitools
-  make install
+  make install -s
   cd $TRAVIS_BUILD_DIR
 fi
